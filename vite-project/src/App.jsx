773a8dcae9cb209380ea52cb17976b36070ea719
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-07 18:57:44 +0530

    Resolve the Error Lines

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,40 @@
 import './App.css'
 import React from 'react'
+import { useState } from "react";
+import { ExpenseContext, MoneyContext } from './Context';
+import Header from './Clint/Header/Header';
+import Right from './Clint/Right/Right';
+import Left from './Clint/Left/Left';
+import { Data } from './Data';
+
+
 
 const App = () => {
+  const [money, setMoney] = useState({
+    balance: 3800,
+    expenses: 1200
+  })
+   const [transactionData, setTransactionData] = useState(Data);
+   
+    const Money = JSON.stringify(money);
+    localStorage.setItem("Income", Money);
+    sessionStorage.setItem("Income", Money);
+    const Expenses = JSON.stringify(transactionData);
+    localStorage.setItem("Expenses", Expenses);
+    sessionStorage.setItem("Expenses", Expenses);
+
+
   return (
-    <div>App</div>
+   <main>
+    <MoneyContext.Provider value={[money, setMoney]}>
+      <ExpenseContext.Provider value={[transactionData, setTransactionData]}>
+      <Header />
+      <Left balance={money.balance} expenses={money.expenses} />
+      <Right transactionData={transactionData} />
+      </ExpenseContext.Provider>
+    </MoneyContext.Provider>
+   </main>
   )
 }
 
-export default App
+export default App;
