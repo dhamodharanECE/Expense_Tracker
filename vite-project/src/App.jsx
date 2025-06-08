fdbe354f46362964121ae53cda9e86b55e06725e
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-08 14:09:16 +0530

    Completed the Expense Tracker Application- Resolve the Context.js in App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import React from 'react'
 import { useState } from "react";
-import { ExpenseContext, MoneyContext } from './Context/Context.js';
 import Header from './Clint/Header/Header';
 import Right from './Clint/Right/Right';
 import Left from './Clint/Left/Left';
 import { Data } from './DataPath/Data';
+import { ExpenseContext, MoneyContext } from './ComponentModel/ComponentPath';
 
 
 
