5c752f6c5e75ce62ed259b617782cfc953b23ff2
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-09 09:58:52 +0530

    Push up all code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import React from 'react'
 import { useState } from "react";
+import { ExpenseContext, MoneyContext } from './ComponentModel/ComponentPath';
+import Header from './Clint/Header/Header';
 import Right from './Clint/Right/Right';
 import Left from './Clint/Left/Left';
 import { Data } from './DataPath/Data';
-import { ExpenseContext, MoneyContext } from './ComponentModel/ComponentPath';
-import Header from './Clint/Header/Header';
 
 
 
@@ -31,7 +31,7 @@ const App = () => {
       <ExpenseContext.Provider value={[transactionData, setTransactionData]}>
        <Header />
       <Left balance={money.balance} expenses={money.expenses} />
-      <Right transactionData={transactionData} />
+      <Right transactionData={transactionData} /> 
       </ExpenseContext.Provider>
     </MoneyContext.Provider>
     
